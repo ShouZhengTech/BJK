e21ad07e87a1f42d0b90781fd71aff58ae4b1d17
fang duan <df1228@gmail.com>
2017-07-03 11:18:43 +0800

    Init

---
+++ src/entry.js
@@ -0,0 +1,14 @@
+// require js files
+require('script-loader!js/vendor/modernizr-2.8.3.min.js')
+
+require('jquery')
+require('js/vendor/bootstrap.min.js')
+require('js/plugins.js')
+require('js/main.js')
+
+
+// require css files
+require('css/bootstrap.min.css')
+require('css/material-design-iconic-font.min.css')
+require('css/plugins.css')
+require('css/style.css')
